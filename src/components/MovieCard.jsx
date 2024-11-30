8845360702a991ed50cab9f31848aa039c04f3eb
Srini <srinismart2@gmail.com>
2024-12-01 01:49:33 +0530

    css transform

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import { IMAGE_URL } from "../utils/constants";
 
 const MovieCard = (props) => {
   return (
-    <div className="w-48 pr-4">
+    <div className="w-48 pr-4 cursor-pointer duration-100 hover:scale-125 ease-out">
       <img
         className=""
         src={IMAGE_URL + props?.posterPath}
