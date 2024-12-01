d665cfc47af75edec3ccbd5921645f0c2f2021e8
Srini <srinismart2@gmail.com>
2024-12-01 14:38:47 +0530

    const

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,8 +3,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGViZjYxZGYyOTk4NWE2ZmYzMDRiZGJlZGUxZmIxMSIsIm5iZiI6MTczMjk4NzYwMi42ODEsInN1YiI6IjY3NGI0YWQyMGRjMmZkYjA1MjNmMTA2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v5wppekBAwEYlGGkzWSv8pBC0FwgXjDQNhmuCvS-fRs",
+    Authorization: "", //TODO
   },
 };
 export const IMAGE_URL = `https://image.tmdb.org/t/p/w500`;
